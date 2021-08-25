ec4ac3b2ae68eeecb38d8e8a6d49f854d2335834
whitewolf8383 <kandt.vandf@gmail.com>
2021-08-24 21:25:08 -0600

    React Basic Components

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Components
+import Heading from "./Heading";
+import List from "./List";
+
+function App() {
+  return (
+  <div>
+    <Heading />
+    <List />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List() {
+  return <ul>
+    <li>Bacon</li>
+    <li>Curry</li>
+    <li>Hotdogs</li>
+  </ul>
+};
+
+export default List;
